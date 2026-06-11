bab7ce36c261c2931b81d566b536a8045baa4652
Jarone Guese <jguese3@rrc.ca>
2026-06-11 17:29:04 -0500

    Added get movie by id function

--- src/assets/apis/allMoviesRepo.ts
+++ src/assets/apis/allMoviesRepo.ts
@@ -6,4 +6,12 @@ export function fetchMovies(): Movie[] {
     return sampleMovies;
 }
 
+// Get movie by id
+export function getMovieById(id: number): Movie {
+    const foundMovie = sampleMovies.find(movie => movie.id === id);
+    if (!foundMovie) {
+        throw new Error(`Movie with id ${id} not found`);
+    }
+    return foundMovie;
+}
 
